c69ad681fa4c45a6b8dc9f1f3b042346bdf8d0b6
Ruz3n <khadkaruzen36@gmail.com>
2022-06-08 13:05:48 +0545

    second commit

--- src/components/signup page/sign.js
+++ src/components/signup page/sign.js
@@ -1,31 +1,90 @@
-import React, { useState } from 'react'
-import InputForm from '../input Form/inputForm';
+import axios from "axios";
+import React, { useState } from "react";
 
 function SignUp() {
+  const url = "https://student-status-system.herokuapp.com/api/v1/student/save";
 
-const [values, setValues] = useState({
-  address:'',
-  contact:'',
-  email:'',
-  fullname:'',
-  password:'',
-  confirmPassword:''
-});
+  const [data, setData] = useState({
+    address: "",
+    contact: "",
+    email: "",
+    fullname: "",
+    password: "",
+  });
+
+  function submit(e) {
+    e.preventDefault();
+    axios
+      .post(url, {
+        address: data.address,
+        contact: data.contact,
+        email: data.email,
+        name: data.fullname,
+        password: data.password,
+      })
+      .then((res) => {
+        console.log("posted");
+      })
+      .catch((err) => {
+        console.log("error: " + err);
+      });
+  }
+
+  function handle(e) {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+    console.log(newdata);
+  }
 
   return (
     <div>
-      <form>
-        <InputForm placeholder="address" />
-        <InputForm placeholder="contact" />
-        <InputForm placeholder="email" />
-        <InputForm placeholder="fullname" />
-        <InputForm placeholder="password" />
-        <InputForm placeholder="confirmPassword" />
-        <button>Submit</button>
+      <p> student form: </p>
+      <form onSubmit={(e) => submit(e)}>
+        <input
+          onChange={(e) => handle(e)}
+          id="address"
+          value={data.address}
+          type="text"
+          placeholder="address"
+        />{" "}
+        <br />
+        <input
+          onChange={(e) => handle(e)}
+          id="contact"
+          value={data.contact}
+          type="text"
+          placeholder="contact"
+        />{" "}
+        <br />
+        <input
+          onChange={(e) => handle(e)}
+          id="email"
+          value={data.email}
+          type="text"
+          placeholder="email"
+        />{" "}
+        <br />
+        <input
+          onChange={(e) => handle(e)}
+          id="fullname"
+          value={data.fullname}
+          type="text"
+          placeholder="fullname"
+        />{" "}
+        <br />
+        <input
+          onChange={(e) => handle(e)}
+          id="password"
+          value={data.password}
+          type="password"
+          placeholder="password"
+        />{" "}
+        <br />
+        <button>submit</button>
       </form>
     </div>
-  )
+  );
 }
 
 export default SignUp;
-
